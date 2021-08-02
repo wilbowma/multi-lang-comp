8d2ca0d3a1ffa29b63d58ab5d6093453eed4db7e
William J. Bowman <wjb@williamjbowman.com>
2021-08-02 16:09:04 -0700

    Tweak demo based on practice run

--- lfcs-2021-seminar-talk/anf-demo.rkt
+++ lfcs-2021-seminar-talk/anf-demo.rkt
@@ -7,6 +7,7 @@
 
 
 ;; Take second path; there's a bug in the first one.
-(stepper anf->+ (term (AS (let ([x (let ([y true]) y)]) x))))
+;(stepper anf->+ (term (AS (let ([x (let ([y true]) y)]) x))))
+(stepper anf->+ (term (AS (let ([x (begin (set! y true) y)]) x))))
 
 (stepper anf->+ (term (AS (+ (if (let ([x #t]) x) 6 7) 1))))
